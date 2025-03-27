cb967ac7c1af9d11cdb62a08df79adf547358719
lelido <17890168+lelido@users.noreply.github.com>
2025-03-27 11:57:54 +0900

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+    let sum = 0;
+    const min = Math.min(n1, n2);
+    const max = Math.max(n1, n2);
 
+    if (min < 0 || !Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR";
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
