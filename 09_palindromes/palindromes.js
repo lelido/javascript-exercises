d56d0d7af9605e3d165f89e9adcac7ccccdddc91
lelido <17890168+lelido@users.noreply.github.com>
2025-03-31 15:13:18 +0900

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const noPunctuation = Array.from(str).filter(letter => !".,!?-:; ".includes(letter)).join("");
+    const reversed = noPunctuation.split("").reverse().join("");
+    return noPunctuation.toLowerCase() === reversed.toLowerCase();
 };
 
 // Do not edit below this line
