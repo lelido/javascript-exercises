5f07e9c02e2b1cc8b8ff4ca516ae995876758239
lelido <17890168+lelido@users.noreply.github.com>
2025-03-31 14:52:38 +0900

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce((mul, num) => mul * num, 1);
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
