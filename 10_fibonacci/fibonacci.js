ed79940ff12956378dc0ed7a88affbabafcecff2
lelido <17890168+lelido@users.noreply.github.com>
2025-03-31 15:38:00 +0900

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    const int = parseInt(num);
 
+    if (int < 0) {
+        return "OOPS";
+    }
+
+    let numA = 1;
+    let numB = 0;
+
+    for (let i = 2; i <= int + 1; i++) {
+        [numA, numB] = [numB, numA + numB];
+    }
+
+    return numB;
 };
 
 // Do not edit below this line
